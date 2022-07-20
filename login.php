bca7f48a1aa2c688174e0b7726b6b2762b5c9e7a
Intruder Developer <59080539+vnsmusyoki@users.noreply.github.com>
2022-07-20 18:50:47 +0300

    start

---
+++ login.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Inkd. - Responsive html template for tattoo artists</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <link href='https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic|Montserrat:400,700'
+        rel='stylesheet' type='text/css'>
+
+</head>
+
+<body>
+
+
+
+    <div class="navigation">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 navbar navbar-default" role="navigation">
+                    <div class="logo">
+                        <a href="#" title="Inkd." class="navbar-brand">Inkd<span>.</span></a>
+                    </div>
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse"
+                            data-target=".navbar-collapse">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                    </div>
+                    <div class="collapse navbar-collapse">
+                        <ul class="nav navbar-nav">
+                            <li>
+                                <a href="index.php" title="Home">
+                                    <span data-hover="Home">Home</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#about" title="About">
+                                    <span data-hover="About">About</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#portfolio" title="Portfolio">
+                                    <span data-hover="Portfolio">Portfolio</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#prices" title="Prices">
+                                    <span data-hover="Prices">Prices</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#contact" title="Contact">
+                                    <span data-hover="Contact">Contact</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="login.php" title="Blog">
+                                    <span data-hover="Login">Login</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div id="contact" class="waypoint">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h3>Welcome</h3>
+
+                    <form role="form" id="contact_form">
+                        <div class="form-group">
+                            <label for="InputName">Name <span>*</span></label>
+                            <input type="text" class="form-control" id="InputName">
+                        </div>
+                        <div class="form-group">
+                            <label for="InputEmail">Email <span>*</span></label>
+                            <input type="email" class="form-control" id="InputEmail">
+                        </div>
+
+                        <button type="submit" class="btn btn-default btn-green">Sign In to My Account</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <p>&copy; 2014 All rights reserved. Inkd.<br />
+                        <a href="#hero" title="Back to top">Back to top</a>
+                    </p>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <script src="js/jQuery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.stellar.min.js"></script>
+    <script src="js/waypoints.min.js"></script>
+    <script src="js/waypoints-sticky.min.js"></script>
+    <script src="https://maps.google.com/maps/api/js?sensor=false&amp;language=en"></script>
+    <script src="js/gmap3.min.js"></script>
+    <script src="js/jquery.queryloader2.min.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/jqueryUI.custom.min.js"></script>
+    <script src="js/timePicker.js"></script>
+    <script src="js/custom.js"></script>
+</body>
+
+</html>
