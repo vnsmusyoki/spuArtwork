6edb87f96bc03b9234e0efc3c1b6f89d11e29c61
Intruder Developer <59080539+vnsmusyoki@users.noreply.github.com>
2022-07-20 20:11:41 +0300

    create artist

---
+++ admin/footer.php
@@ -0,0 +1,12 @@
+<link rel="stylesheet" type="text/css" href="vendors/styles/style.css">
+<!-- js -->
+<script src="vendors/scripts/core.js"></script>
+<script src="vendors/scripts/script.min.js"></script>
+<script src="vendors/scripts/process.js"></script>
+<script src="vendors/scripts/layout-settings.js"></script>
+<script src="src/plugins/apexcharts/apexcharts.min.js"></script>
+<script src="src/plugins/datatables/js/jquery.dataTables.min.js"></script>
+<script src="src/plugins/datatables/js/dataTables.bootstrap4.min.js"></script>
+<script src="src/plugins/datatables/js/dataTables.responsive.min.js"></script>
+<script src="src/plugins/datatables/js/responsive.bootstrap4.min.js"></script>
+<script src="vendors/scripts/dashboard.js"></script>
